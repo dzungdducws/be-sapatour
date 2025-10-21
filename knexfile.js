f442d36eeb6e38bcd1e3456e7b77b9848d40d517
dungnd <dzungdducws@gmail.com>
2025-10-21 23:36:17 +0700

    .

--- knexfile.js
+++ knexfile.js
@@ -5,12 +5,8 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: 'db.qfrxbyekwdlsoqefmxhd.supabase.co',
-      user: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASSWORD || 'Letm3se3!uRcode',
-      database: process.env.DB_NAME || 'postgres',
-      port: 5432,
-      ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
     },
     migrations: {
       directory: './migrations'
